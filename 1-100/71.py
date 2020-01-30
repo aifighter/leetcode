85611892a8203ce8c5a7db62813c33864c5f456d
xuchao.lu <xuchao.lu@hypereal.com>
2020-01-30 11:34:16 +0800

    Add solution no.71

--- 1-100/71.py
+++ 1-100/71.py
@@ -1,21 +1,13 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        # 连续的/化成一个
-        i = 1
-        while i < len(path):
-            if path[i] == '/' and path[i - 1] == '/':
-                path = path[:i] + path[i+1:]
-            else:
-                i += 1
-        # 去除最后的/
-        if len(path) > 1 and path[-1] == '/':
-            path = path[:-1]
         # 重新组合，删除"."，".."和前面的同归于尽 
-        paths = path[1:].split('/')
+        paths = path.split('/')
         i = 0
         while i < len(paths):
             if i < 0:
                 i = 0
+            elif paths[i] == '':
+                del paths[i]
             elif paths[i] == '.':
                 del paths[i]
             elif paths[i] == ".." and i == 0:
