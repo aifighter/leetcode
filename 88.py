0743cb4a1bd7b1877092fbe9167d7a8c5fb19ed7
xuchao.lu <xuchao.lu@hypereal.com>
2020-02-04 19:54:39 +0800

    Add solution no.88

---
+++ 88.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        a = m - 1
+        b = n - 1
+        c = m + n - 1
+        while 1:
+            if a >= 0 and b >= 0:
+                if nums1[a] > nums2[b]:
+                    nums1[c] = nums1[a]
+                    a -= 1
+                else:
+                    nums1[c] = nums2[b]
+                    b -= 1
+            elif a >= 0:
+                break
+            elif b >= 0:
+                nums1[c] = nums2[b]
+                b -= 1
+            else:
+                break
+            c -= 1
